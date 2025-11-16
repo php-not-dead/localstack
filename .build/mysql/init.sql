fd027c4f93fdc589131ce616a2b65942ad3515b7
Vitalij Vladimirov <vitalij.vladimirov@opay.eu>
2025-11-16 21:00:00 +0200

    Add `say_labas` DB schema

--- .build/mysql/init.sql
+++ .build/mysql/init.sql
@@ -3,4 +3,7 @@ CREATE USER 'php_not_dead'@'%' IDENTIFIED WITH mysql_native_password BY 'passwor
 CREATE DATABASE IF NOT EXISTS cto_for_sale;
 GRANT ALL PRIVILEGES ON cto_for_sale.* TO 'php_not_dead'@'%';
 
+CREATE DATABASE IF NOT EXISTS say_labas;
+GRANT ALL PRIVILEGES ON say_labas.* TO 'php_not_dead'@'%';
+
 FLUSH PRIVILEGES;
